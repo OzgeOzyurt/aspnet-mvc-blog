6f9f945b0a1bba4ab593b81fe43461c53ec0a644
OzgeOzyurt <ozgeozyurt00@gmail.com>
2023-10-13 18:48:52 +0300

    Yönetici Paneli Kurulumu ve Temel İşlevlerin Eklenmesi gerçekleştirildi.

--- App.Web.Mvc/AspNetMvcBlog/Program.cs
+++ App.Web.Mvc/AspNetMvcBlog/Program.cs
@@ -1,4 +1,6 @@
 using App.Business;
+using App.Business.Services.Abstract;
+using App.Business.Services;
 using App.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConStr")));
+
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ISettingService, SettingService>();
+builder.Services.AddScoped<IPageService, PageService>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
@@ -28,6 +36,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "MyArea",
+	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
